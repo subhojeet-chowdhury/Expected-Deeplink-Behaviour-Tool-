e0230700b1951e2d8920547ba88cf4ab9a3d4308
Subhojeet Chowdhury <subhojeetchowdhury98@gmail.com>
2022-07-02 00:37:21 +0530

    minor errors

--- script.js
+++ script.js
@@ -102,11 +102,10 @@ function showInputValues() {
   selectors.forEach((selector) => {
     arr.push(selector.value.split("_").join(" "));
   });
-  let str = `Showing results for <b>${arr[0]}</b>(chaneel), <b>${arr[1]}</b>
+  let str = `Showing results for <b>${arr[0]}</b>(channel), <b>${arr[1]}</b>
   (partner/placement),
-  <b>${arr[2]}</b>(platform), <b>${arr[3]}</b>(App Status), <b>${arr[4]}</b>
-  IDFA Status (source),
-  <b>${arr[5]}</b>IDFA Status(Client app)`;
+  <b>${arr[2]}</b>(platform), <b>${arr[3]}</b>(App Status), <b>${arr[4]}</b>(IDFA Status (source)),
+  <b>${arr[5]}</b>(IDFA Status(Client app))`;
 
   document.querySelector(".current_selected_inputs").innerHTML = str;
 }
